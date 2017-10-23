b553ad32b2b6ad319bd6e703f7befe90596cab46
Chris Johnson and Luke Bertram <chris@>
2017-10-23 09:52:51 -0700

    add array transcription logic to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
-  var favArr = [];
   $('#favoriteForm').submit(function(event){
+    event.preventDefault();
+    var favArr = [];
     var favorite1 = $('#favoriteInput1').val();
     var favorite2 = $('#favoriteInput2').val();
     var favorite3 = $('#favoriteInput3').val();
     favArr.push(favorite1, favorite2, favorite3);
-    alert(favArr);
+    var pos1 = favArr.splice(1, 1, favorite1, favorite2, favorite3); // 1,2,3 //
+    favArr.unshift(pos1);
+    //display favArray
+    $('#favorite-list').empty();
+    favArr.forEach(function(fav) {
+      $('#favorite-list').append("<li>" + fav + "</li>");
+    });
+
   });
 });
